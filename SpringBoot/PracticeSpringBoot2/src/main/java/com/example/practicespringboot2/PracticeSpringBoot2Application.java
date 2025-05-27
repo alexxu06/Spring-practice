0dae0188f5f1654e2ef4be81184741a14f7eeaaf
Alex <alexxu0308@gmail.com>
2025-05-26 23:47:10 -0600

    updated SpringBoot

---
+++ SpringBoot/PracticeSpringBoot2/src/main/java/com/example/practicespringboot2/PracticeSpringBoot2Application.java
@@ -0,0 +1,13 @@
+package com.example.practicespringboot2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticeSpringBoot2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracticeSpringBoot2Application.class, args);
+	}
+
+}
---
+++ SpringBoot/PracticeSpringBoot2/src/test/java/com/example/practicespringboot2/PracticeSpringBoot2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.practicespringboot2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticeSpringBoot2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
