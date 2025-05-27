3d363fe9133a6e5ae545ec170cd13b89011d5b65
Alex <alexxu0308@gmail.com>
2025-05-27 01:20:11 -0600

    update practice2

---
+++ SpringBoot/PracticeSpringBoot2/src/main/java/com/example/practicespringboot2/services/LoginProcessor.java
@@ -0,0 +1,52 @@
+package com.example.practicespringboot2.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.annotation.RequestScope;
+
+@Component
+@RequestScope
+public class LoginProcessor {
+
+    private final LoggedUserManagementService loggedUserManagementService;
+    private final LoginCountService loginCountService;
+
+    private String username;
+    private String password;
+
+    @Autowired
+    public LoginProcessor(LoggedUserManagementService loggedUserManagementService, LoginCountService loginCountService) {
+        this.loggedUserManagementService = loggedUserManagementService;
+        this.loginCountService = loginCountService;
+    }
+
+    public boolean login() {
+        loginCountService.increment();
+        String username = this.getUsername();
+        String password = this.getPassword();
+
+        boolean loggedResult = false;
+        if ("alex".equals(username) && "123".equals(password)) {
+            loggedResult = true;
+            loggedUserManagementService.setUsername(username);
+        }
+
+        return loggedResult;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+}
